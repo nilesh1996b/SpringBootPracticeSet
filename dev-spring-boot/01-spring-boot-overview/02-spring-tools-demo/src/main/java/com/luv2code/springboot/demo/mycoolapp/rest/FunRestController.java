0149e5f32b68961fd161245898def4a35660d6a4
nilesh <nileshbind.nb@gmail.com>
2023-03-15 14:29:48 +0530

    modified3

--- dev-spring-boot/01-spring-boot-overview/02-spring-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/02-spring-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -11,4 +11,23 @@ public class FunRestController {
     {
         return "Hello World!";
     }
+
+
+//    expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout()
+    {
+        return "Run a hard 5K";
+    }
+
+
+    //    expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune()
+    {
+        return "Today is your lucky day ";
+    }
+
 }
