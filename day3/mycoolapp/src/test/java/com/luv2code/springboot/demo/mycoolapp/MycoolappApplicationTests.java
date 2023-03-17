c4c6287a3c06f311de5fcc5ebe01bac13e4424c8
nilesh <nileshbind.nb@gmail.com>
2023-03-17 09:18:01 +0530

    add

--- day3/mycoolapp/src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
+++ day3/mycoolapp/src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nilesh.demo;
+package com.luv2code.springboot.demo.mycoolapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MycoolappApplicationTests {
 
 	@Test
 	void contextLoads() {
