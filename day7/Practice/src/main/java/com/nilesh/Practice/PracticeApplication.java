8beb945be4fddcf1d590f3e01aca8e18695d78c4
nilesh <nileshbind.nb@gmail.com>
2023-03-24 11:24:54 +0530

    firstAdd

---
+++ day7/Practice/src/main/java/com/nilesh/Practice/PracticeApplication.java
@@ -0,0 +1,23 @@
+package com.nilesh.Practice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args)
+	{
+		return runner->{
+
+		};
+	}
+
+}
